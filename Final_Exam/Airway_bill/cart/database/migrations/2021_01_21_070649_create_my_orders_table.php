ee1defe7b1e46d4dd482f117bfb1529c1163cd95
liong7658 <liongjessy@gmail.com>
2021-01-21 15:08:54 +0800

    1

---
+++ Final_Exam/Airway_bill/cart/database/migrations/2021_01_21_070649_create_my_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMyOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('my_orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('orderId');
+            $table->string('email');
+            $table->date('orderDate');
+            $table->string('receiverName');
+            $table->string('address');
+            $table->integer('postCode');
+            $table->string('courierId');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('my_orders');
+    }
+}
